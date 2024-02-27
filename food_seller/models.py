22bd7abb188d8665068b40c1680aefe854ed976b
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-02-27 10:55:21 +0800

    feat: modified some file modified : food_seller/models.py add some model class modified : urls.py add some route modified : food_seller/admin.py register new model to admin page

--- food_seller/models.py
+++ food_seller/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Food(models.Model) :
+
+    food_name = models.CharField(max_length=50,null=False)
+    food_desc = models.TextField()
+    food_price = models.FloatField(default=0,null=False)
+    food_stock = models.IntegerField(default=0,null=False)
+
+    def __str(self) :
+        return "{}. {}".format(self.id,self.food_name)
