7a4abcbf99241468ae086d1eef9a49e0c7eb3e57
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-02-28 12:00:14 +0800

    feat: add new file and modified some files
    modified:   food_seller/templates/food_seller/detail.html
    modified:   food_seller/templates/food_seller/index.html
    modified:   food_seller/urls.py
    modified:   food_seller/views.py
    new file:   static/css/styles.css
    new file:   static/img/buritto.jpg
    new file:   static/img/french_fries.jpg
    new file:   static/img/hamburger.jpg
    new file:   static/img/hotdog.jpg
    modified:   templates/base.html
    modified:   templates/snippets/navbar.html
    modified:   templates/snippets/styles.html

--- food_seller/urls.py
+++ food_seller/urls.py
@@ -6,5 +6,6 @@ app_name='food_seller'
 urlpatterns = [
     path('',views.index,name='index'),
     # detail food
+    # path()
     path('<str:name>',views.food_detail,name='food_detail')
 ]
--- food_seller/views.py
+++ food_seller/views.py
@@ -1,24 +1,29 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
+from django.http import HttpResponse
 from .models import Food
 
 # Create your views here.
+
+# function for index of food_seller
 def index(request) :
     food_list = Food.objects.all()
     ctx = {
         'title' : 'We sell food and beverages',
-        'food_list' : food_list,
+        'food_list' : food_list, 
     }
     print(food_list)
     return render(request,'food_seller/index.html',ctx)
 
-
+# function for detail of food
 def food_detail(request,name) :
-    food_detail = Food.objects.get(food_name=name)
-    print(food_detail)
+    # get food based on food_name
+    detail = Food.objects.get(food_name=name)
+    print(detail)
 
     ctx = {
         'title' : 'Item Details',
+        'detail' : detail,
     }
-    return render(request,'food_seller/detail.html',ctx)
+    return render(request, 'food_seller/detail.html', ctx)
+    
 
