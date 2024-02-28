47eb22f206a6a4064faefc3188e45811ed7bd137
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-02-28 19:12:40 +0800

    feat : add new files and modified some files new file:   users/templates/users/index.html, for index view modified:   users/urls.py, add route to index modified:   users/views.py, add function index

--- users/views.py
+++ users/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    ctx = {
+        'title' : 'User Page',
+    }
+    return render(request,'users/index.html',ctx)
