d8097eaa012adb971fce2d7e5dd22f00c7df5788
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-02-29 09:41:31 +0800

    feat: modified 3 files modified:   mywebsite/settings.py adding LOGIN_REDIRECT_URL to redirect user when user success to login modified:   static/css/styles.css adding css setting for login templates modified:   users/templates/users/login.html

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -138,3 +138,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redirect page when user success to login
+LOGIN_REDIRECT_URL = 'users:index'
