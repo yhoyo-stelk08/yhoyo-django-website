a38420d33bbb4b0892531dae652e5fd693d52ea3
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-02-29 09:11:20 +0800

    feat: modified urls.py modified:   mywebsite/urls.py

--- mywebsite/urls.py
+++ mywebsite/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from . import views
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
