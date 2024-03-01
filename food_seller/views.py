70fde7f7c92a17e737090612e14bac9d43ca1ee1
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-03-01 08:01:17 +0800

    feat: edit 3 files modified:   food_seller/templates/food_seller/detail.html change food_id into pk modified:   food_seller/urls.py change the path to use class based views modified:   food_seller/views.py add class based views to for CRUD

--- food_seller/views.py
+++ food_seller/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
 from .models import Food
 from .forms import FoodForm
 
@@ -16,13 +16,6 @@ def index(request) :
     # print(food_list)
     return render(request,'food_seller/index.html',ctx)
 
-# Class Based View for index
-class FoodListView(ListView):
-    model = Food
-    template_name = "food_seller/index.html"
-    context_object_name = 'food_list'
-    extra_context = {'title': 'We sell food and beverages'}
-
 # function for detail of food
 def food_detail(request,food_id) :
     # get food based on food id
@@ -35,12 +28,6 @@ def food_detail(request,food_id) :
     }
     return render(request, 'food_seller/detail.html', ctx)
 
-class FoodDetailView(DetailView):
-    model = Food
-    template_name = "food_seller/detail.html"
-    context_object_name = 'detail'
-    extra_content = {'title' : 'Food Detail'}
-    
 def food_create(request):
     form_data = FoodForm(request.POST or None)
 
@@ -85,3 +72,59 @@ def food_delete(request,food_id):
     }
 
     return render(request,'food_seller/food_delete.html',ctx)
+
+# Class Based View for index
+class FoodListView(ListView):
+    model = Food
+    template_name = "food_seller/index.html"
+    context_object_name = 'food_list'
+    extra_context = {'title': 'We sell food and beverages'}
+
+# class based view for show detail
+class FoodDetailView(DetailView):
+    model = Food
+    template_name = "food_seller/detail.html"
+    context_object_name = 'detail'
+    extra_content = {'title' : 'Food Detail'}
+
+# class based view to create food
+class FoodCreateView(CreateView):
+    template_name = 'food_seller/food_form.html'
+    form_class = FoodForm
+    success_url = '/food_seller/'  # Redirects to food_seller:index upon successful form submission
+    extra_context = {'title': 'Add Food Form'}
+
+    def form_valid(self, form):
+        """
+        If the form is valid, save the associated model.
+        """
+        form.save()
+        return super().form_valid(form)
+
+# class based view for edit food
+class FoodEditView(UpdateView):
+    model = Food
+    form_class = FoodForm
+    template_name = 'food_seller/food_form.html'
+    success_url = '/food_seller/'  # Redirects to food_seller:index upon successful form submission
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Food Edit Form'
+        return context
+
+# class based view to delete food
+class FoodDeleteView(DeleteView):
+    model = Food
+    template_name = "food_seller/food_delete.html"
+    success_url = "/food_seller/" # redirect to food_seller:index if the food is sucessfuly deleted
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Food Delete  Form'
+        return context
+    
+    def post(self, request, *args, **kwargs):
+        food = self.get_object()
+        food.delete()
+        return redirect(self.success_url)
